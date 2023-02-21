6abde53d8cc3648f5dfe28491bb4d6f006ce80dc
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-02-20 23:38:33 -0300

    refactor to use dependency injection on controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,8 +3,12 @@
 require_relative '../actions/auth/sign_in'
 
 class AuthController < ApplicationController
+  def initialize
+    super
+    @sign_in_action = Actions::Auth::SignInAction.new
+  end
   def sign_in
-    status, error, user = Actions::Auth::SignInAction.new.call(email: params[:email], password: params[:password]).values_at(
+    status, error, user = @sign_in_action.call(email: params[:email], password: params[:password]).values_at(
       :status, :error, :user
     )
 
