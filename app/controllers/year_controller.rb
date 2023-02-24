c3c551992b3b835e17164835653890fe34dd99d9
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-02-24 20:46:50 -0300

    Create a post route to insert new year with months

---
+++ app/controllers/year_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require_relative '../actions/year/create_year'
+require_relative '../actions/year/create_months'
+require_relative '../actions/auth/handle_jwt'
+
+class YearController < ApplicationController
+  def initialize
+    super
+    @create_year_action = Actions::Year::CreateYearAction.new
+    @create_months_in_year_action = Actions::Year::CreateMonthsAction.new
+    @handle_jwt = Actions::Auth::HandleJwtAction.new
+  end
+
+  def create
+    user = @handle_jwt.decode_into_user(token: request.headers['Authorization'])
+
+    p user
+
+    result = @create_year_action.call(name: params[:name], user:)
+
+    return render json: { error: result[:error] }, status: result[:status] unless result[:error].nil?
+
+    months_result = @create_months_in_year_action.call(year_id: result[:year][:id])
+
+    unless months_result[:error].nil?
+      return render json: { error: months_result[:error] },
+                    status: months_result[:status]
+    end
+
+    render status: result[:status]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@
 Rails.application.routes.draw do
   post 'auth/sign_in', to: 'auth#sign_in'
   post 'auth/sign_up', to: 'auth#sign_up'
+  post 'year', to: 'year#create'
 end
