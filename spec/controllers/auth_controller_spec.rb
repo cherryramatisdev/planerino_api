cbf91dfd3b305f74be3ad441a8f2f5a8d613f15b
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-02-28 22:49:46 -0300

    Update legacy tests to use rspec patterns

--- spec/controllers/auth_controller_spec.rb
+++ spec/controllers/auth_controller_spec.rb
@@ -7,40 +7,39 @@ describe AuthController, type: :request do
 
     # TODO: Discover how to grab path from rails variable.
     post '/auth/sign_in', params: { email: 'test@test.com', password: 'test' }
-    assert_response :success
 
-    sut = ActiveSupport::JSON.decode @response.body
+    sut = JSON.parse(response.body)
 
-    assert sut['name'] == 'test'
-    assert sut['email'] == 'test@test.com'
+    expect(response).to have_http_status(:success)
+    expect(sut['token']).to be_a(String)
   end
 
   it 'should not authenticate with wrong params' do
     post auth_sign_in_path, params: { email: 'test@test.com', password: 'test' }
-    assert_response :bad_request
 
-    sut = ActiveSupport::JSON.decode @response.body
+    sut = JSON.parse(response.body)
 
-    assert_not sut['error'].empty?
+    expect(response).to have_http_status(:bad_request)
+    expect(sut['error']).to_not be_empty
   end
 
   it 'should create new user with correct params' do
     post auth_sign_up_path,
          params: { name: 'test', email: 'test@test.com', password: 'test', password_confirmation: 'test' }
-    assert_response :success
 
-    sut = ActiveSupport::JSON.decode @response.body
+    sut = JSON.parse(response.body)
 
-    assert sut['name'] == 'test'
-    assert sut['email'] == 'test@test.com'
+    expect(response).to have_http_status(:success)
+    expect(sut['name']).to eq 'test'
+    expect(sut['email']).to eq 'test@test.com'
   end
 
   it 'should not create a new user with wrong params' do
     post auth_sign_up_path, params: { name: 'test' }
-    assert_response :bad_request
 
-    sut = ActiveSupport::JSON.decode @response.body
+    sut = JSON.parse(response.body)
 
-    assert_not sut['error'].empty?
+    expect(response).to have_http_status(:bad_request)
+    expect(sut['error']).to_not be_empty
   end
 end
