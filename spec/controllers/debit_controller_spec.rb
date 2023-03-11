2c149dc8d81fa9ac3f7c94fe242431f6286251d6
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-03-11 20:48:56 -0300

    Improve test

--- spec/controllers/debit_controller_spec.rb
+++ spec/controllers/debit_controller_spec.rb
@@ -21,12 +21,16 @@ describe DebitController, type: :request do
       year = Year.create(name: '2023', user:)
       month = Month.create(name: Faker::Date.between(from: '2022-01-01', to: '2023-01-01').strftime('%B'), year:)
 
-      post '/debit', params: { title: 'title', price: 0.0, paid: false, payer:, month: }
+      post '/debit', params: { title: 'title', price: 10.0, paid: false, payer_id: payer.id, month_id: month.id }
 
       body = JSON.parse(response.body)
 
       expect(response).to have_http_status(:created)
-      expect(body['name']).to eq('John Doe')
+      expect(body['title']).to eq('title')
+      expect(body['price']).to eq(10.0)
+      expect(body['paid']).to be_falsy
+      expect(body['payer_id']).to eq(payer.id)
+      expect(body['month_id']).to eq(month.id)
     end
   end
 end
